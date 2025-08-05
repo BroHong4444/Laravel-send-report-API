10dbbbe98554633a292feee7b95b60c579288095
Heng Hong <hongofficial4444@gmail.com>
2025-08-05 15:31:10 +0700

    add /vendor to .vercelignore

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => env('APP_NAME', 'Laravel send report API'),
 
     /*
     |--------------------------------------------------------------------------
