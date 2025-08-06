5f6bc16d4683b9dabe1654de7775a9b242723d83
Heng Hong <hongofficial4444@gmail.com>
2025-08-06 11:37:27 +0700

    upate error

--- routes/api.php
+++ routes/api.php
@@ -4,8 +4,4 @@ use App\Http\Controllers\ReportController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-// Route::post('/send-report', [ReportController::class, 'index']);
-
-Route::middleware('api')->group(function () {
-    Route::post('/send-report', [ReportController::class, 'sendTelegram']);
-});
+Route::post('/send-report', [ReportController::class, 'sendTelegram']);
